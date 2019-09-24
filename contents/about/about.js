9363d0fe3b5369c8914d542927bc9c1912ede3aa
Paul Mowat <paul.mowat@oneadvanced.com>
2019-09-24 19:37:46 +0100

    Updating about

--- contents/about/about.js
+++ contents/about/about.js
@@ -15,7 +15,7 @@ class About extends React.Component {
             <p>I have strong problem solving skills and am an advocate of best practices and coding standards. I believe if your going to do something, then do it to the best of your abilities.</p>
             <p>I enjoy working within a team and have experience leading teams of developers using traditional and agile methodologies to meet tight deadlines.</p>
             <p>During my spare time, I also seek to continually improve my skills and knowledge and as such I enjoy learning about new emerging technologies. I'm always playing around with emerging technologies and frameworks.</p>
-            <p>My other interests include photography, watching movies and spending time with my wife and son.</p>
+            <p>My other interests include photography, watching movies, football and spending time with my wife and son.</p>
           </div>
         </div>
       </MainInner>
