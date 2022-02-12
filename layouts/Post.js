cae8da4052ee7a3c8f2f1c65474374db63cb87aa
Paul Mowat <paul@paulmowat.co.uk>
2022-02-12 12:24:45 +0000

    feat: add prev and next blog links to posts

--- layouts/Post.js
+++ layouts/Post.js
@@ -1,3 +1,4 @@
+import Link from '@/components/Link'
 import PageTitle from '@/components/PageTitle'
 import PageWrapper from '@/components/PageWrapper'
 import { BlogSEO } from '@/components/SEO'
@@ -10,7 +11,7 @@ import ViewCounter from 'components/ViewCounter'
 
 const postDateTemplate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 
-export default function Post ({ children, frontMatter }) {
+export default function Post ({ children, frontMatter, next, prev }) {
   const { slug, date, title, tags } = frontMatter
 
   const pageUrl = `${siteMetadata.siteUrl}/blog/${slug}`
@@ -19,89 +20,89 @@ export default function Post ({ children, frontMatter }) {
       <BlogSEO {...frontMatter} url={pageUrl} title={title} />
       <article>
         <div className='divide-y'>
-          <header className='pt-6 pb-6'>
-            <div className='space-y-1 text-center'>
-              <dl className='space-y-10'>
-                <div>
-                  <dt className='sr-only'>Published on</dt>
-                  <dd className='text-base font-medium leading-6 '>
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
-              <div>
-                <PageTitle>{title}</PageTitle>
-              </div>
-              <div className='py-2 xl:py-3'>
-                <div className='text-xs tracking-wide '>
-                  <div className='text-center '>
-                    {tags.map((tag) => (
-                      <Tag key={tag} text={tag} />
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className='py-2'>
-                <div className='text-xs tracking-wide '>
-                  <div className='text-center'>
-                    <p>{frontMatter.readingTime.text.replace('min', 'minute')}{' • '}<ViewCounter slug={frontMatter.slug} /></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </header>
           <div
             className='divide-y xl:divide-y-0'
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
             <div className='divide-y xl:pb-0'>
-              <div className='pt-6 pb-8 prose max-w-none'>{children}</div>
-              <div className='pb-8 prose max-w-none'>
-                <div className='flex flex-col items-center justify-center pt-6 space-x-4 space-y-4 xl:flex-row xl:items-baseline'>
-                  <p className='m-0'>Useful article? Please share it with your friends.</p>
+              <header className='pt-6 pb-6'>
+                <div className='space-y-1 text-center'>
+                  <dl className='space-y-10'>
+                    <div>
+                      <dt className='sr-only'>Published on</dt>
+                      <dd className='text-base font-medium leading-6 '>
+                        <time dateTime={date}>
+                          {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                        </time>
+                      </dd>
+                    </div>
+                  </dl>
+                  <div>
+                    <PageTitle>{title}</PageTitle>
+                  </div>
+                  <div className='py-2 xl:py-3'>
+                    <div className='text-xs tracking-wide '>
+                      <div className='text-center '>
+                        {tags.map((tag) => (
+                          <Tag key={tag} text={tag} />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className='py-2'>
+                    <div className='text-xs tracking-wide '>
+                      <div className='text-center'>
+                        <p>{frontMatter.readingTime.text.replace('min', 'minute')}{' • '}<ViewCounter slug={frontMatter.slug} /></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </header>
+              <div className='pt-6 pb-6 prose max-w-none'>{children}</div>
+              <div className='pb-6 prose max-w-none'>
+                <div className='flex flex-col items-center justify-center pt-6 space-x-4 space-y-4 xl:space-y-0 xl:flex-row xl:items-baseline'>
+                  <p className='m-0'>Useful post? Please share it with your friends.</p>
                   <ShareButton kind='twitter' label='Share on Twitter' title={title} href={pageUrl} />
                   <ShareButton kind='facebook' label='Share on Facebook' title={title} href={pageUrl} />
                   <ShareButton kind='linkedin' label='Share on LinkedIn' title={title} href={pageUrl} />
                 </div>
                 {/* <div className='flex justify-center'>
-                  <p>Get the latest articles. Sign up to the <a href='/newsletter'>newsletter</a>.</p>
+                  <p>Get the latest posts. Sign up to the <a href='/newsletter'>newsletter</a>.</p>
                 </div> */}
 
               </div>
               <div className='pt-6'>
                 <Comments frontMatter={frontMatter} />
               </div>
+              <footer className='pt-6'>
+                <div className='divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700'>
+                  {(next || prev) && (
+                    <div className='flex justify-between py-2'>
+                      {prev && (
+                        <div>
+                          <h2 className='text-xs tracking-wide uppercase'>
+                            Previous Post
+                          </h2>
+                          <div className='text-blue-500 hover:text-blue-400'>
+                            <Link href={`/blog/${prev.slug}`}>{prev.title}</Link>
+                          </div>
+                        </div>
+                      )}
+                      {next && (
+                        <div>
+                          <h2 className='text-xs tracking-wide uppercase'>
+                            Next Post
+                          </h2>
+                          <div className='text-blue-500 hover:text-blue-400'>
+                            <Link href={`/blog/${next.slug}`}>{next.title}</Link>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </div>
+              </footer>
             </div>
-            {/* <footer>
-              <div className='text-sm font-medium leading-5 divide-gray-700 xl:divide-y xl:col-start-1 xl:row-start-2'>
-                {(next || prev) && (
-                  <div className='flex justify-between py-4 xl:block xl:space-y-8 xl:py-8'>
-                    {prev && (
-                      <div>
-                        <h2 className='text-xs tracking-wide uppercase'>
-                          Previous Article
-                        </h2>
-                        <div className='text-blue-500 hover:text-blue-400'>
-                          <Link href={`/blog/${prev.slug}`}>{prev.title}</Link>
-                        </div>
-                      </div>
-                    )}
-                    {next && (
-                      <div>
-                        <h2 className='text-xs tracking-wide uppercase'>
-                          Next Article
-                        </h2>
-                        <div className='text-blue-500 hover:text-blue-400'>
-                          <Link href={`/blog/${next.slug}`}>{next.title}</Link>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                )}
-              </div>
-            </footer> */}
           </div>
         </div>
       </article>
